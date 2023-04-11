58967419d9d38513d1eb169b1b034d9dee2981f5
Yu Cheng Li <ychengli11065@gmail.com>
2023-04-10 23:49:56 -0700

    finished education component

--- src/components/Education.js
+++ src/components/Education.js
@@ -14,11 +14,45 @@ class Education extends Component {
 
     handleChange = (e) => {
         const { name, value } = e.target
-        
+        this.setState({ [name]: value})  
+    };
 
+    render() {
+        const { university, degree, startDate, endDate } = this.state;
+
+        return (
+            <div>
+                <input
+                    type="text"
+                    name="university"
+                    placeholder="University"
+                    value={university}
+                    onChange={this.handleChange}
+                />
+                <input
+                    type="text"
+                    name="degree"
+                    placeholder="Degree"
+                    value={degree}
+                    onChange={this.handleChange}
+                />
+                <input
+                    type="date"
+                    name="startDate"
+                    placeholder="Start date"
+                    value={startDate}
+                    onChange={this.handleChange}
+                />
+                <input
+                    type="date"
+                    name="endDate"
+                    placeholder="End Date"
+                    value={endDate}
+                    onChange={this.handleChange}
+                />
+            </div>
+        );
     }
-
-
 }
 
 export default Education
