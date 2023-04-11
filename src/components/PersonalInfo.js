7042fb5e064599ac0a3689c7f8aedc3ae66ea42b
Yu Cheng Li <ychengli11065@gmail.com>
2023-04-10 20:45:11 -0700

    Created first component personal info

---
+++ src/components/PersonalInfo.js
@@ -0,0 +1,92 @@
+import React, {Component} from "react";
+
+
+class PersonalInfo extends Component {
+    constructor() {
+        super()
+        this.state = {
+            firstName: "",
+            lastName: "",
+            major: "",
+            year: "",
+            phone: "",
+            email: "",
+            linkedin: "",
+            github: "",
+        };
+    }
+
+    handleChange = (e) => {
+        const { name, value } = e.target;
+        this.setState({ [name]: value});
+    };
+
+    render() {
+        const { firstName, lastName, major, year, phone, email, linkedin, github } = this.state;
+
+        return (
+            <div>
+                <h2>Personal Info</h2>
+                <input
+                    type="text"
+                    name="firstName"
+                    placeholder="First Name"
+                    value={firstName}
+                    onChange={this.handleChange}
+                />
+                <input
+                    type="text"
+                    name="lastName"
+                    placeholder="Last Name"
+                    value={lastName}
+                    onChange={this.handleChange}
+                />
+                <input
+                    type="text"
+                    name="major"
+                    placeholder="Major"
+                    value={major}
+                    onChange={this.handleChange}
+                />
+                <input
+                    type="text"
+                    name="year"
+                    placeholder="Year"
+                    value={year}
+                    onChange={this.handleChange}
+                />
+                <input
+                    type="tel"
+                    name="phone"
+                    placeholder="Phone Number"
+                    value={phone}
+                    onChange={this.handleChange}
+                />
+                <input
+                    type="text"
+                    name="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={this.handleChange}
+                />
+                 <input
+                    type="text"
+                    name="linkedin"
+                    placeholder="Linkedin"
+                    value={linkedin}
+                    onChange={this.handleChange}
+                />
+                 <input
+                    type="text"
+                    name="github"
+                    placeholder="Github"
+                    value={github}
+                    onChange={this.handleChange}
+                />
+            </div>
+        );
+    }
+}
+
+export default PersonalInfo
+
