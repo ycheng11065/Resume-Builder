7ac36053e597cf93d8a84940c9ad2d3d45d84260
Yu Cheng Li <ychengli11065@gmail.com>
2023-04-17 22:20:46 -0700

    connecting components to super class

--- src/components/WorkExperience.js
+++ src/components/WorkExperience.js
@@ -1,104 +1,70 @@
-import React, { Component } from "react";
+import React from "react";
+import DynamicComponents from "./DynamicComponents";
 
-class WorkExperience extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      workExperience: [],
-      inputValues: {},
-    };
-  }
-
-  handleChange = (workId) => (e) => {
-    const { name, value } = e.target;
-    this.setState({ 
-      inputValues: {
-        ...this.state.inputValues,
-        [workId]: {
-          ...this.state.inputValues[workId],
-          [name]: value,
-        },
-      },  
-    });
-  };
-
-  addWorkExperience = () => {
-    const { inputValues, workExperience } = this.state;
-    const workExperienceAmount = workExperience.length;
-    const workId = `Work-${workExperienceAmount + 1}`;
-
-    this.setState({
-      workExperience: [ ...workExperience, workId ],
-      inputValues: {
-        ...inputValues,
-        [workId]: {
-          position: "",
-          company: "",
-          startDate: "",
-          endDate: "",
-          location: "",
-          description: "",
-        },
+class WorkExperience extends DynamicComponents {
+  constructor(props) {
+    super(props, {
+      itemName: "WorkExperience",
+      title: "Work Experience",
+      defaultInputValues: {
+        position: "",
+        company: "",
+        startDate: "",
+        endDate: "",
+        location: "",
+        description: "",
       },
     });
-  };
+  }
 
-  render() {
-    const { workExperience, inputValues } = this.state;
+
+  renderInputs(workId, inputValues, handleChange) {
 
     return (
       <div>
-        <h2>Work Experiences</h2>
-        <button onClick={this.addWorkExperience}>Add Work Experience</button>
-        {workExperience.map(workId => (
-          <div key={workId}>
-            <input
-              type="text"
-              name="position"
-              placeholder="Position"
-              value={inputValues[workId]["position"]}
-              onChange={this.handleChange(workId)}
-            />
-            <input
-              type="text"
-              name="company"
-              placeholder="Company"
-              value={inputValues[workId]["company"]}
-              onChange={this.handleChange(workId)}
-            />
-            <input
-              type="date"
-              name="startDate"
-              placeholder="Start date"
-              value={inputValues[workId]["startDate"]}
-              onChange={this.handleChange(workId)}
-            />
-            <input
-              type="date"
-              name="endDate"
-              placeholder="End date"
-              value={inputValues[workId]["endDate"]}
-              onChange={this.handleChange(workId)}
-            />
-            <input
-              type="text"
-              name="location"
-              placeholder="Location"
-              value={inputValues[workId]["location"]}
-              onChange={this.handleChange(workId)}
-            />
-            <textarea
-              name="description"
-              placeholder="Description"
-              value={inputValues[workId]["description"]}
-              onChange={this.handleChange(workId)}
-              rows="4"
-              cols="50"
-            />
-          </div>
-        ))  
-        }
+        <input
+          type="text"
+          name="position"
+          placeholder="Position"
+          value={inputValues[workId]["position"]}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="company"
+          placeholder="Company"
+          value={inputValues[workId]["company"]}
+          onChange={handleChange}
+        />
+        <input
+          type="date"
+          name="startDate"
+          placeholder="Start date"
+          value={inputValues[workId]["startDate"]}
+          onChange={handleChange}
+        />
+        <input
+          type="date"
+          name="endDate"
+          placeholder="End date"
+          value={inputValues[workId]["endDate"]}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="location"
+          placeholder="Location"
+          value={inputValues[workId]["location"]}
+          onChange={handleChange}
+        />
+        <textarea
+          name="description"
+          placeholder="Description"
+          value={inputValues[workId]["description"]}
+          onChange={handleChange}
+          rows="4"
+          cols="50"
+        />
       </div>
     );
   }
