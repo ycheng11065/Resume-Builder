b7718c00a81aab56cac573fba7223973c74ec98f
Yu Cheng Li <ychengli11065@gmail.com>
2023-04-17 22:07:32 -0700

    usual commit

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import DynamicComponents from "./DynamicComponents";
 
 class Education extends DynamicComponents {
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 
 class Skills extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
       category: "",
