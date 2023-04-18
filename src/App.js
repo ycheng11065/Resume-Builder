ef7e8d200099a9f4baa8bb23296e914d52c8639b
Yu Cheng Li <ychengli11065@gmail.com>
2023-04-18 14:54:17 -0700

    update component props in app

--- src/App.js
+++ src/App.js
@@ -7,15 +7,33 @@ import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      PersonalInfo: {},
+      Education: {},
+      Projects: {},
+      Skills: {},
+      WorkExperience: {},
+    }
+  }
+
+  updateStates = (newData, component) => {
+    this.setState({
+      [component]: newData,
+    });
+  };
+
   render() {
     return (
       <div>
         <h1>Resume Builder</h1>
-        <PersonalInfo />
-        <Education />
-        <WorkExperience />
-        <Projects />
-        <Skills />
+        <PersonalInfo onUpdate={(newData) => this.updateStates(newData, "PersonalInfo")} />
+        <Education onUpdate={(newData) => this.updateStates(newData, "Education")} />
+        <WorkExperience onUpdate={(newData) => this.updateStates(newData, "Projects")} />
+        <Projects onUpdate={(newData) => this.updateStates(newData, "Skills")} />
+        <Skills onUpdate={(newData) => this.updateStates(newData, "WorkExperience")} />
       </div>
     );
   }
