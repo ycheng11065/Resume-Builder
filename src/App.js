f693a7fe7e37500959a9705b75d44d9e6af6eb54
Yu Cheng Li <ychengli11065@gmail.com>
2023-04-19 18:13:54 -0700

    usual commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import WorkExperience from "./components/WorkExperience";
 import Education from "./components/Education";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
-import ResumePreview from "./components/ResumePreview"
+import ResumePreview from "./components/ResumePreview";
 
 class App extends Component {
   constructor(props) {
@@ -18,16 +18,22 @@ class App extends Component {
       projects: {},
       skills: {},
       workExperience: {},
-    }
+    };
   }
 
   generateSample = () => {
     const educationId = uuidv4();
     const projectIds = [];
+    const workIds = [];
 
     for (let i = 0; i < 4; i++) {
       const currId = uuidv4();
       projectIds.push(currId);
+    }
+
+    for (let i = 0; i < 2; i++) {
+      const currId = uuidv4();
+      workIds.push(currId);
     }
 
     const sampleData = {
@@ -49,19 +55,56 @@ class App extends Component {
             degree: "Bachelor of Science",
             startDate: "Sept 2020",
             endDate: "April 2026",
-          }
+          },
         },
       },
       projects: {
-        items: [],
-        inputValues: {},
+        items: [projectIds[0], projectIds[1], projectIds[2], projectIds[3]],
+        inputValues: {
+          [projectIds[0]]: {
+            name: "Online Marketplace Platform",
+            technology: "React, Redux, Node.js, Express, PostgreSQL",
+            completionDate: "June 2022",
+            description: `• Developed an online marketplace platform for buying and selling goods
+            • Implemented user authentication and authorization with JWT
+            • Designed a RESTful API to handle user and product data
+            • Utilized Redux for efficient state management`,
+          },
+          [projectIds[1]]: {
+            name: "Real-time Chat Application",
+            technology: "React, Socket.IO, Node.js, Express",
+            completionDate: "January 2022",
+            description: `• Created a real-time chat application using WebSockets
+            • Implemented private and group chat functionality
+            • Used Socket.IO for seamless communication between server and clients
+            • Designed a responsive user interface using Material-UI`,
+          },
+          [projectIds[2]]: {
+            name: "Weather Forecast Web App",
+            technology: "Vue.js, Vuex, OpenWeatherMap API",
+            completionDate: "August 2021",
+            description: `• Developed a weather forecast web app that displays current weather and 7-day forecast
+            • Integrated OpenWeatherMap API to fetch real-time weather data
+            • Utilized Vuex for centralized state management
+            • Implemented geolocation to automatically fetch weather data based on user's location`,
+          },
+          [projectIds[3]]: {
+            name: "Task Management System",
+            technology: "Angular, Firebase, Firestore",
+            completionDate: "April 2021",
+            description: `• Built a task management system to help users organize their tasks
+            • Implemented CRUD operations for tasks and projects
+            • Utilized Firebase for user authentication and Firestore for data storage
+            • Applied responsive design principles for an optimal user experience on various devices`,
+          },
+        },
       },
       skills: {
         items: [],
         inputValues: {},
       },
       workExperience: {},
-    }
+    };
 
     this.setState(sampleData);
   };
@@ -77,11 +120,21 @@ class App extends Component {
       <div>
         <div>
           <h1>Resume Builder</h1>
-          <PersonalInfo onUpdate={(newData) => this.updateStates(newData, "personalInfo")} />
-          <Education onUpdate={(newData) => this.updateStates(newData, "education")} />
-          <WorkExperience onUpdate={(newData) => this.updateStates(newData, "workExperience")} />
-          <Projects onUpdate={(newData) => this.updateStates(newData, "projects")} />
-          <Skills onUpdate={(newData) => this.updateStates(newData, "skills")} />
+          <PersonalInfo
+            onUpdate={(newData) => this.updateStates(newData, "personalInfo")}
+          />
+          <Education
+            onUpdate={(newData) => this.updateStates(newData, "education")}
+          />
+          <WorkExperience
+            onUpdate={(newData) => this.updateStates(newData, "workExperience")}
+          />
+          <Projects
+            onUpdate={(newData) => this.updateStates(newData, "projects")}
+          />
+          <Skills
+            onUpdate={(newData) => this.updateStates(newData, "skills")}
+          />
         </div>
         <div>
           <ResumePreview
