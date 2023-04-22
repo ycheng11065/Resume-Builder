d25d2493cd7463de864ccd816e9df93e91031278
Yu Cheng Li <ychengli11065@gmail.com>
2023-04-21 22:18:20 -0700

    usual commit

--- src/components/WorkExperience.js
+++ src/components/WorkExperience.js
@@ -1,3 +1,4 @@
+import "./WorkExperience.css"
 import React from "react";
 import DynamicComponents from "./DynamicComponents";
 
@@ -21,55 +22,62 @@ class WorkExperience extends DynamicComponents {
     return (
       <div className="Work-Root">
         <div className="Work-Input">
-          <input
-            className="Work-Position"
-            type="text"
-            name="position"
-            placeholder="Position"
-            value={inputValues[workId]["position"]}
-            onChange={handleChange}
-          />
-          <input
-            className="Work-Company"
-            type="text"
-            name="company"
-            placeholder="Company"
-            value={inputValues[workId]["company"]}
-            onChange={handleChange}
-          />
-          <input
-            className="Work-Start-Date"
-            type="text"
-            name="startDate"
-            placeholder="Start date"
-            value={inputValues[workId]["startDate"]}
-            onChange={handleChange}
-          />
-          <input
-            className="Work-End-Date"
-            type="text"
-            name="endDate"
-            placeholder="End date"
-            value={inputValues[workId]["endDate"]}
-            onChange={handleChange}
-          />
-          <input
-            className="Work-Location"
-            type="text"
-            name="location"
-            placeholder="Location"
-            value={inputValues[workId]["location"]}
-            onChange={handleChange}
-          />
-          <textarea
-            className="Work-Description"
-            name="description"
-            placeholder="Description"
-            value={inputValues[workId]["description"]}
-            onChange={handleChange}
-            rows="4"
-            cols="50"
-          />
+          <div className="First-Row">
+            <input
+              className="Work-Position"
+              type="text"
+              name="position"
+              placeholder="Position"
+              value={inputValues[workId]["position"]}
+              onChange={handleChange}
+            />
+            <input
+              className="Work-Company"
+              type="text"
+              name="company"
+              placeholder="Company"
+              value={inputValues[workId]["company"]}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="Second-Row">
+            <input
+              className="Work-Start-Date"
+              type="text"
+              name="startDate"
+              placeholder="Start date"
+              value={inputValues[workId]["startDate"]}
+              onChange={handleChange}
+            />
+            <input
+              className="Work-End-Date"
+              type="text"
+              name="endDate"
+              placeholder="End date"
+              value={inputValues[workId]["endDate"]}
+              onChange={handleChange}
+            />
+            <input
+              className="Work-Location"
+              type="text"
+              name="location"
+              placeholder="Location"
+              value={inputValues[workId]["location"]}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="Third-Row">
+            <textarea
+              className="Work-Description"
+              name="description"
+              placeholder="Description"
+              value={inputValues[workId]["description"]}
+              onChange={handleChange}
+              rows="4"
+              cols="50"
+            />
+          </div>
         </div>        
       </div>
     );
