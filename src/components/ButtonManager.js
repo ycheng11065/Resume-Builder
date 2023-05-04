5f5af73d9317cb24f871d15cf4ce69b912db418c
Yu Cheng Li <ychengli11065@gmail.com>
2023-05-04 16:37:45 -0700

    usual commit

--- src/components/ButtonManager.js
+++ src/components/ButtonManager.js
@@ -10,10 +10,23 @@ import { SlGraduation } from "react-icons/sl";
 
 
 class ButtonManager extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            activeButton: "profile",
+        }
+    }
+
     handleButtonClick = (buttonType) => {
+        this.setState({ activeButton: buttonType })
         if (this.props.onButtonClick) {
             this.props.onButtonClick(buttonType);
         }
+    }
+
+    getButtonClass = (buttonType) => {
+        return this.state.activeButton === buttonType ? "Active-Button" : "";
     }
 
 
@@ -21,19 +34,19 @@ class ButtonManager extends Component {
 
         return (
             <div className="Button-Manager-Root">
-                <button className="Button-Profile" onClick={() => this.handleButtonClick("profile")}> 
+                <button className= {`Button-Profile ${this.getButtonClass("profile")}`} onClick={() => this.handleButtonClick("profile")}> 
                     <CgProfile className="Button-Icon"/>
                 </button>   
-                <button className="Button-Education" onClick={() => this.handleButtonClick("education")}> 
+                <button className= {`Button-Education ${this.getButtonClass("education")}`} onClick={() => this.handleButtonClick("education")}> 
                     <SlGraduation className="Button-Icon"/>
                 </button>   
-                <button className="Button-Work" onClick={() => this.handleButtonClick("work")}> 
+                <button className= {`Button-Work ${this.getButtonClass("work")}`} onClick={() => this.handleButtonClick("work")}> 
                     <CgWorkAlt className="Button-Icon"/>
                 </button>   
-                <button className="Button-Project" onClick={() => this.handleButtonClick("project")}> 
+                <button className= {`Button-Project ${this.getButtonClass("project")}`} onClick={() => this.handleButtonClick("project")}> 
                     <RiLightbulbLine className="Button-Icon"/>
                 </button>   
-                <button className="Button-Skills" onClick={() => this.handleButtonClick("skill")}> 
+                <button className= {`Button-Skills ${this.getButtonClass("skill")}`} onClick={() => this.handleButtonClick("skill")}> 
                     <TbTools className="Button-Icon"/>
                 </button>   
             </div>
